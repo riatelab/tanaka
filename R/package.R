cdf3db3cd42e9df858ca525bf258262e0793e674
rCarto <timothee.giraud@cnrs.fr>
2019-03-13 12:22:33 +0100

    package doc

--- R/package.R
+++ R/package.R
@@ -1,7 +1,12 @@
-#' @title tanaka
-#' @name tanaka
-#' @rdname tanaka
+#' @title Tanaka Contours Package
+#' @name tanaka-package
+#' @rdname tanaka-package
 #' @description
-#' Tanaka pkg
+#' The tanaka package is a simplified implementation of the Tanaka method.
+#' Also called "relief contours method", "illuminated contour method", or
+#' "shaded contour lines method", the Tanaka method enhances the representation
+#' of topography on a map using shaded contour lines. North-west white contours
+#' represent illuminated topography and south-east black contours represent
+#' shaded topography.
 #' @docType package
 NULL
